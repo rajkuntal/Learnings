8a94141dce16da9f23f6d6bce1ec07cdd9eca7b4
rajkuntal <raj.kuntal09@gmail.com>
2015-10-30 23:39:33 +0530

    delete all

--- Programs/FullCoreJava/src/MyStringToInteger.java
+++
@@ -1,8 +0,0 @@
-public class MyStringToInteger {
- 
-    public static void main(String a[]){
-        String str = "23";
-        Integer i = Integer.valueOf(str);
-        System.out.println("The integer value: "+i);
-    }
-}
